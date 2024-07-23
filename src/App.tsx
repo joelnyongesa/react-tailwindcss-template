bdf10bd16fb0622f0a1d5a287a8dc80f60473bd9
Joel Nyongesa <joelnyongesa148@gmail.com>
2024-07-23 16:28:31 +0300

    Initial Project setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1
+        className='text-3xl text-center text-red-700'
+      >
+        Welcome to Vite with TailwindCSS and React
+      </h1>
+    </>
+  )
+}
+
+export default App
